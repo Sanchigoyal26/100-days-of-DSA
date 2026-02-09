967ccefd78c5c7c541807524691f31ab8104f3eb
Sanchigoyal26 <sanchigoyal282@gmail.com>
2026-02-09 23:32:58 +0530

    Initial commit

---
+++ day1.cpp
@@ -0,0 +1,35 @@
+//Write a C program to insert an element x at a given 1-based position pos 
+//in an array of n integers. Shift existing elements to the right to make space.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of elements in array";
+    cin>>n;
+    //input array
+    int arr[n+1];
+    for(int i=0;i<n;i++){
+        cout<<"enter "<<i+1<<" element"<<" ";
+        cin>>arr[i];
+    }
+    //input position and number
+    int pos,num;
+    cout<<"enter the number to add ";
+    cin>>num;
+    cout<<"enter the position ";
+    cin>>pos;
+    //put that number in position
+    int temp=arr[n-1];
+    for(int i=n;i>pos;i--){
+        arr[i-1]=arr[i-2];
+    }
+    arr[n]=temp;
+    arr[pos]=num;
+    //printing final array
+    cout<<"new array";
+    for(int i=0;i<n+1;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+
+}
