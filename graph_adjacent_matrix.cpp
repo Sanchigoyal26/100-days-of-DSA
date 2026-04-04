01ea1eacb9808665f4f5b723c2999ebcbb5f5c6a
Sanchigoyal26 <sanchigoyal282@gmail.com>
2026-04-04 18:49:55 +0530

    added graph_adjacent_matrix.cpp

---
+++ graph_adjacent_matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n, vector<int>(n, 0));
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u][v] = 1;      // edge u -> v
+        adj[v][u] = 1;      // remove this line if graph is directed
+    }
+
+    // Print adjacency matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
