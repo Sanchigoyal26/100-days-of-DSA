d32b2eb932ae7c2dd7a99cfa665fc9a9596c30ef
Sanchigoyal26 <sanchigoyal282@gmail.com>
2026-04-26 14:13:12 +0530

    added floyd_warshall.cpp

---
+++ floyd_warshall.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> dist(n, vector<int>(n));
+
+    // input
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+
+            if(dist[i][j] == -1) {
+                dist[i][j] = 1e9; // INF
+            }
+        }
+    }
+
+    // Floyd-Warshall
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dist[i][k] < 1e9 && dist[k][j] < 1e9) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    // output
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] == 1e9)
+                cout << -1 << " ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
