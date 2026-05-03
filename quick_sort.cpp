45dd23b699221a5a71471aaf071650b74f52e02e
Sanchigoyal26 <sanchigoyal282@gmail.com>
2026-05-03 11:05:30 +0530

    added quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Partition function (Lomuto)
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]); // place pivot correctly
+    return i + 1;
+}
+
+// Quick Sort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);  // left
+        quickSort(arr, pi + 1, high); // right
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    // Print sorted array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
